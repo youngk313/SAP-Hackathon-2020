29d25c67e25c75e7c79980332bdc05c6ec7c07bf
yogiduzit <yverma2@my.bcit.ca>
2020-04-16 15:41:24 -0700

    Add basic authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import '../App.css';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Grid from '@material-ui/core/Grid';
+
 import Charts from './Charts';
+import ProfilePage from './ProfilePage';
 
-export default function App() {
-  return (
-    <div className="App">
-        <Grid container spacing ={2}>
-          <Grid item xs = {12}>
+// Authentication
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import UserProvider from '../providers/UserProvider';
 
-          </Grid>
-          <Grid item xs = {12}>
 
-            <Charts/>
+const App = () => {
+	return (
+		<UserProvider>
+		<div className="App">
+				<Grid container spacing ={2}>
+					<Grid item xs = {12}>
 
-          </Grid>
-      </Grid>
-    </div>
-  );
+					</Grid>
+					<Grid item xs = {12}>
+
+						<Charts/>
+
+					</Grid>
+			</Grid>
+			<Router>
+				<SignUp exact path='/signUp'/>
+				<SignIn path='/'/>
+			</Router>
+		</div>
+		</UserProvider>
+	);
 }
+
+export default App;
 
 
 
---
+++ src/components/ProfilePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { auth } from '../firebase';
+
+const ProfilePage = () => {
+
+}
+
+export default ProfilePage;
