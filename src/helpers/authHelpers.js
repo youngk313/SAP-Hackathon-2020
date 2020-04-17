d9d1f130a7a7c35823ef6cde2dc4582c77f02b64
yogiduzit <yverma2@my.bcit.ca>
2020-04-17 08:51:24 -0700

    Can add profile picture

--- src/helpers/authHelpers.js
+++ src/helpers/authHelpers.js
@@ -17,7 +17,7 @@ export const createUserWithEmailAndPasswordHandler = async (event, auth, email, 
     try {
         await auth.setPersistence(firebase.auth.Auth.Persistence.SESSION);
         await auth.createUserWithEmailAndPassword(email, password);
-        await addDisplayName(displayName);
+        await addDisplayName(auth, displayName);
         history.push('/');
     } catch (error) {
         console.error(error);
@@ -27,7 +27,7 @@ export const createUserWithEmailAndPasswordHandler = async (event, auth, email, 
 export const addDisplayName = async (auth, displayName) => {
     if (!!displayName) {
         try {
-            await auth.currentUser.updateProfile({displayName});
+            await auth.currentUser.updateProfile({displayName, photoURL: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F7%2F7c%2FProfile_avatar_placeholder_large.png&imgrefurl=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AProfile_avatar_placeholder_large.png&tbnid=gp8LDHLwxDk-WM&vet=12ahUKEwj8-5uS5-_oAhWUFjQIHekbAhEQMygHegUIARCGAg..i&docid=xZyB11jLps67TM&w=360&h=360&q=avatar%20photo&hl=en-GB&ved=2ahUKEwj8-5uS5-_oAhWUFjQIHekbAhEQMygHegUIARCGAg'});
         } catch (error) {
             console.error(error);
         }
